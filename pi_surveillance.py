e0d250d06dd8fdeb7e1a69a752cd517d9f03e788
JimvanB <jvanbiezen@gmail.com>
2017-04-01 20:36:39 +0200

    dev --> fixed and check

--- pi_surveillance.py
+++ pi_surveillance.py
@@ -109,7 +109,7 @@ for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True
 		cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
 					cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 		# check to see if enough time has passed between uploads
-		if (conf["use_dropbox"] and timestamp - lastUploaded).seconds >= conf["min_upload_seconds"]:
+		if (conf["use_dropbox"]) and ((timestamp - lastUploaded.seconds) >= conf["min_upload_seconds"]):
 			# increment the motion counter
 			motionCounter += 1
 
